25da0703119d69ffe6cb41496e33ebd2951f423d
arab-son <mohammedmudathirmuhammad@gmail.com>
2025-04-29 05:12:42 -0700

    Add files via upload
    this is a sample of facebook

---
+++ Facebook Clone_files/facebook clone.js
@@ -0,0 +1,4 @@
+document.getElementById('signup-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Sign Up functionality is not implemented in this frontend clone.');
+});
